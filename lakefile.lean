759d8c273dee57320287dd0e33bde70197e31649
Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>
2023-02-01 20:31:24 -0500

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «quiz» {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_lib «Quiz» {
+  -- add library configuration options here
+}
